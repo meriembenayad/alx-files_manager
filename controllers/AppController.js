8e7a1e17f4ee5dbfcabe955189f0153eb3a785a5
meriembenayad <meriem.benayad@gmail.com>
2024-05-13 21:14:15 +0100

    Task 2: First API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,1 +1,18 @@
-#!/usr/bin/node
+import dbClient from "../utils/db";
+import redisClient from "../utils/redis";
+
+class AppController {
+  static getStatus(req, res) {
+    const redisAlive = redisClient.isAlive();
+    const dbAlive = dbClient.isAlive();
+    res.status(200).send({redis: redisAlive, db: dbAlive});
+  }
+
+  static async getStats(req, res) {
+    const usersNumber = dbClient.nbUsers();
+    const filesNumber = dbClient.nbFiles();
+    res.status(200).send({users: usersNumber, files: filesNumber});
+  }
+}
+
+export default AppController;
--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+import express from 'express';
+import routes from './routes/index';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
