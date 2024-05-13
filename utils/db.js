9fb6e494058808b89616d1f92654e8dced408886
meriembenayad <meriem.benayad@gmail.com>
2024-05-13 20:31:26 +0100

    Task 1: MongoDB utils

--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,30 @@
-#!/usr/bin/node
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+
+    this.client = new MongoClient(url);
+    this.client.db(database);
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const collection = this.client.db().collection('users');
+    return collection.countDocuments();
+  }
+
+  async nbFiles() {
+    const collection = this.client.db().collection('files');
+    return collection.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
