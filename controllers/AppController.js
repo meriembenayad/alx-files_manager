abb56b1ad7a087167d6bd2ea6492644911c3e442
meriembenayad <meriem.benayad@gmail.com>
2024-05-13 21:15:55 +0100

    Task 2: First API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,17 +1,17 @@
-import dbClient from "../utils/db";
-import redisClient from "../utils/redis";
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class AppController {
   static getStatus(req, res) {
     const redisAlive = redisClient.isAlive();
     const dbAlive = dbClient.isAlive();
-    res.status(200).send({redis: redisAlive, db: dbAlive});
+    res.status(200).send({ redis: redisAlive, db: dbAlive });
   }
 
   static async getStats(req, res) {
     const usersNumber = dbClient.nbUsers();
     const filesNumber = dbClient.nbFiles();
-    res.status(200).send({users: usersNumber, files: filesNumber});
+    res.status(200).send({ users: usersNumber, files: filesNumber });
   }
 }
 
