910ec77749308d03aee99cd211d85cfce9839854
meriembenayad <meriem.benayad@gmail.com>
2024-05-13 01:01:47 +0100

    Task 0: Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,1 +1,45 @@
-#!/usr/bin/node
+import redis from 'redis';
+
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.error(error);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (error, reply) => {
+        if (error) reject(error);
+        else resolve(reply);
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (error, reply) => {
+        if (error) reject(error);
+        else resolve(reply);
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (error, reply) => {
+        if (error) reject(error);
+        else resolve(reply);
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
