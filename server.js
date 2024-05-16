137991f5a196e86052627afaed7162a62edaab2e
ThamiBa <baladithami@gmail.com>
2024-05-16 01:53:47 +0100

    Files_Manager

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 import express from 'express';
-import routes from './routes/index';
+import router from './routes';
 
 const app = express();
-const port = process.env.PORT || 5000;
+app.use(express.json({ limit: '100mb' }));
+app.use(router);
+app.listen(process.env.PORT || 5000);
 
-app.use(express.json());
-app.use('/', routes);
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+export default app;
